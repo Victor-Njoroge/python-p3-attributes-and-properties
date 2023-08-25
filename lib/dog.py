7d008c0ca3dbe3566b8c18cee2ac9a51ad9a4c48
Victor Njoroge <victornjoroge2020@gmail.com>
2023-08-25 19:32:52 +0300

    done

--- lib/dog.py
+++ lib/dog.py
@@ -12,4 +12,27 @@ APPROVED_BREEDS = [
 ]
 
 class Dog:
-    pass
+    def __init__(self, name, breed):
+        if 1 <= len(name) <= 25:
+            self.name = name
+            self._breed = breed  
+
+    def get_breed(self):
+        return self._breed
+
+    def set_breed(self, breed):
+        if breed in APPROVED_BREEDS:
+            self._breed = breed
+        else:
+            print("Breed must be in list of approved breeds.")
+
+    breed = property(get_breed, set_breed)
+
+# Creating an instance of Dog
+Snoopy = Dog("Blue", "Pointer")
+
+# Accessing the name attribute
+print(Snoopy.name)  # This line was missing a print statement
+
+# Accessing the breed attribute
+print(Snoopy.breed)
--- lib/person.py
+++ lib/person.py
@@ -16,4 +16,28 @@ APPROVED_JOBS = [
 ]
 
 class Person:
-    pass
+    def __init__(self,name,job):
+            self._name=name
+            self._job=job
+    def get_name (self):
+          return self._name
+    def set_name (self, name):
+        if 1 <=  len(name) <= 25:
+              self._name=name
+        else :
+              print("Name must be string under 25 characters.")
+    
+    def get_job (self):
+          return self._job
+    def set_job(self,job):
+          if job in APPROVED_JOBS:
+                self._job=job
+          else :
+                print("Job must be in list of approved jobs.")
+    
+    job=property (get_job,set_job)
+    name=property (get_name,set_name)
+
+Person1=Person("Victor","ITC")
+
+print(Person1.job)
